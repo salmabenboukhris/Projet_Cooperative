e583ad5d106a360276e4af1bf1d0a651e36c753e
BAJADDA Asma <a.bajadda2955@uca.ac.ma>
2026-02-26 22:50:38 +0000

    ajouter des comentaires
    Updated comments in AgriculteurTest.java to be in French.

--- src/presentation/AgriculteurTest.java
+++ src/presentation/AgriculteurTest.java
@@ -12,29 +12,29 @@ public class AgriculteurTest {
 
         System.out.println("===== TEST AGRICULTEUR =====");
 
-        // 🔹 CREATE
+        // creation des agriculteurs
         Agriculteur a1 = new Agriculteur("Ahmed", "Taroudant", "0600000000");
         service.create(a1);
 
         Agriculteur a2 = new Agriculteur("Salma", "Agadir", "0611111111");
         service.create(a2);
 
-        // 🔹 FIND ALL
+        //trouver tous les agriculteurs
         System.out.println("\nListe des agriculteurs :");
         List<Agriculteur> list = service.findAll();
         for (Agriculteur a : list) {
             System.out.println(a);
         }
 
-        // 🔹 UPDATE
+        // modifier agriculteur
         a1.setNom("Ahmed Updated");
         service.update(a1);
 
-        // 🔹 FIND BY ID
+        // trouver un agriculteur par son id
         System.out.println("\nRecherche par ID :");
         System.out.println(service.findById(a1.getIdAgriculteur()));
 
-        // 🔹 DELETE
+        // supprimer 
         service.delete(a2);
 
         System.out.println("\nAprès suppression :");
